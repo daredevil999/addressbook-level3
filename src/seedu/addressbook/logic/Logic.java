63e3de49f2febeee972f2fc5747775d824c3554c
KIANMING-PC\KiANMiNG <kianming@live.com.sg>
2016-09-16 09:31:50 +0800

    Edited the comments for the execute method in Logic.java

--- src/seedu/addressbook/logic/Logic.java
+++ src/seedu/addressbook/logic/Logic.java
@@ -76,7 +76,7 @@ public class Logic {
     }
 
     /**
-     * Executes the command, updates storage, and returns the result.
+     * Executes the command, updates storage if the data has been updated, and returns the result.
      *
      * @param command user command
      * @return result of the command
